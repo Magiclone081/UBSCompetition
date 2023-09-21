7cbb54c390cf8fba81f033319ab1891de10fed9c
Magiclone081 <siuhowoon@gmail.com>
2023-09-22 02:36:51 +0800

    lazy-developer

--- lazy-developer/LazyDeveloper.js
+++ lazy-developer/LazyDeveloper.js
@@ -8,7 +8,7 @@ exports.LazyDeveloper = (jsonMap) => {
     let resultMap = new Map();
 
     for (let statement of statements) {
-        
+
         const tokens = statement.split(".");
 
         // Check if each token is navigable in classes
@@ -31,7 +31,7 @@ exports.LazyDeveloper = (jsonMap) => {
             let wordCandidates = getAllPossibilities(normalizedClasses, currentLocation);
             let wordsStartWithToken = wordCandidates.filter(str => str.startsWith(lastToken));
             wordsStartWithToken.sort();
-            const top5wordList = wordsStartWithToken.slice(0,5);
+            const top5wordList = wordsStartWithToken.slice(0, 5);
             resultMap.set(statement, top5wordList.length === 0 ? [""] : top5wordList);
         } else {
             // Statement does not correspond to any navigable class, no suggestion should be
@@ -45,12 +45,12 @@ exports.LazyDeveloper = (jsonMap) => {
 
 const getAllPossibilities = (normalizedClasses, currentLocation) => {
     if (shouldResolve(currentLocation)) {
-      currentLocation = getNextLocation(normalizedClasses, null, currentLocation.toString());
+        currentLocation = getNextLocation(normalizedClasses, null, currentLocation.toString());
     }
     // If current location is null, no traversal has been done previously, so keys
     // of normalized class should be returned
     if (currentLocation === null) {
-      return Array.from(normalizedClasses.keys());
+        return Array.from(normalizedClasses.keys());
     }
     if (currentLocation instanceof Array) {
         // Assumption 2 made as per header
@@ -61,26 +61,30 @@ const getAllPossibilities = (normalizedClasses, currentLocation) => {
         // Assumption 1 made as per header
         return Array.from(Object.keys(currentLocation));
     }
-    
-    
+
+
     // If current location is not a map (like String), no possibilities should be
     // returned
     return [""];
-  }
+}
 
 const shouldResolve = (currentLocation) => {
     return currentLocation instanceof String && !currentLocation.equals("");
-  }
+}
 
 const getNextLocation = (normalizedClasses, currentLocation, token) => {
+    if (currentLocation instanceof Array) {
+        const getResult = normalizedClasses.get(tokenLookup);
+        return (getResult === undefined) ? null : getResult;
+    }
     if (currentLocation instanceof Object || currentLocation == null) {
-      // Assumption 3 made as per header
-      const tokenLookup = currentLocation === null ? token : Object.getOwnPropertyDescriptors(currentLocation).token;
-      if (tokenLookup === null) {
-        return null;
-      }
-      const getResult = normalizedClasses.get(tokenLookup);
-      return (getResult === undefined) ? null : getResult;
+        // Assumption 3 made as per header
+        const tokenLookup = currentLocation === null ? token : Object.getOwnPropertyDescriptors(currentLocation).token;
+        if (tokenLookup === null) {
+            return null;
+        }
+        const getResult = normalizedClasses.get(tokenLookup);
+        return (getResult === undefined) ? null : getResult;
     }
     return null;
-  }
+}
