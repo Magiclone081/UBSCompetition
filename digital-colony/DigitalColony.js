97adef7b53f8bd098bcab5bdb34a6ddac350be1d
Magiclone081 <siuhowoon@gmail.com>
2023-09-23 12:47:43 +0800

    lazy-developer

--- digital-colony/DigitalColony.js
+++ digital-colony/DigitalColony.js
@@ -6,7 +6,7 @@ exports.DigitalColony = (generationRequest) => {
     for(let request of generationRequest){
         const totalGeneration = request['generations'];
         let colony = request['colony'].split('').flatMap(value => parseInt(value));
-        console.log(JSON.stringify(colony));
+        //console.log(JSON.stringify(colony));
         let colonyWeight = colony.reduce((currSum, i) => currSum + i, 0);
         for(let currentGeneration = 0; currentGeneration < totalGeneration; currentGeneration++){
             //console.log(`current iteration: ${currentGeneration}`);
@@ -19,9 +19,9 @@ exports.DigitalColony = (generationRequest) => {
                 newColony.push((colonyWeight + signature)%10);
                 
             }
-            console.log(`new colony${colonyWeight}`)
+            //console.log(`new colony${colonyWeight}`)
             newColony.push(colony[colony.length-1]);
-            console.log(`entire colony${newColony}`)
+            //console.log(`entire colony${newColony}`)
             colony = newColony;
             colonyWeight = colony.reduce((currSum, i) => currSum + i, 0);
            // console.log(`${colonyWeight}`)
