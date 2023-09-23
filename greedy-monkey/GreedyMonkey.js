cd4bdd44ce8dd4dbeaf089070a23b89915267fa4
Magiclone081 <siuhowoon@gmail.com>
2023-09-23 16:06:33 +0800

    lazy-developer

--- greedy-monkey/GreedyMonkey.js
+++ greedy-monkey/GreedyMonkey.js
@@ -8,31 +8,35 @@ exports.GreedyMonkey = (InputMap) => {
     // let volumeUsed = 0;
     // let currentScore = 0;
     
-    
+    let dp = Array(allFruits.length+1).fill().map(() => Array(noOfWeightMonkeyCarry).fill().map(() => Array(basketVolume)));
 
 
-    return recursiveGreedyMonkey(allFruits, noOfWeightMonkeyCarry, basketVolume);
+    return recursiveGreedyMonkey(allFruits, noOfWeightMonkeyCarry, basketVolume, dp);
 }
 
 
 
-const recursiveGreedyMonkey = (allFruits, noOfRemainingWeightMonkeyCarry, noOfRemainingBasketVolume) =>{
+const recursiveGreedyMonkey = (allFruits, noOfRemainingWeightMonkeyCarry, noOfRemainingBasketVolume, dp) =>{
     if(allFruits.length <= 1){
         if(allFruits[0][0] <=noOfRemainingWeightMonkeyCarry && allFruits[0][1] <=noOfRemainingBasketVolume){
-            return allFruits[0][2];
+            return dp[allFruits.length][noOfRemainingWeightMonkeyCarry][noOfRemainingBasketVolume] = allFruits[0][2];
         }
         return 0;
+    }
+
+    if(dp[allFruits.length][noOfRemainingWeightMonkeyCarry][noOfRemainingBasketVolume] != undefined){
+        return dp[allFruits.length][noOfRemainingWeightMonkeyCarry][noOfRemainingBasketVolume];
     }
 
     const lastFruit = allFruits.pop();
     
     if(lastFruit[0] > noOfRemainingWeightMonkeyCarry || lastFruit[1] > noOfRemainingBasketVolume){
-        return recursiveGreedyMonkey(allFruits, noOfRemainingWeightMonkeyCarry, noOfRemainingBasketVolume);
+        return dp[allFruits.length][noOfRemainingWeightMonkeyCarry][noOfRemainingBasketVolume] = recursiveGreedyMonkey(allFruits, noOfRemainingWeightMonkeyCarry, noOfRemainingBasketVolume);
     }
     
     const scoreForAddingFruitToBasket = recursiveGreedyMonkey(allFruits, noOfRemainingWeightMonkeyCarry-lastFruit[0], noOfRemainingBasketVolume-lastFruit[1]) + lastFruit[2];
     const scoreForNotAddingFruitToBasket = recursiveGreedyMonkey(allFruits, noOfRemainingWeightMonkeyCarry, noOfRemainingBasketVolume);
     
-    return Math.max(scoreForAddingFruitToBasket, scoreForNotAddingFruitToBasket);
+    return dp[allFruits.length][noOfRemainingWeightMonkeyCarry][noOfRemainingBasketVolume] = Math.max(scoreForAddingFruitToBasket, scoreForNotAddingFruitToBasket);
 
 }
